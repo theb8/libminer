fe82915a24166b45cc4817c7a91c30b7f51af380
AlexTheb <theb8@hotmail.com>
2024-11-05 12:16:08 -0500

    add testthat and initial tests

---
+++ tests/testthat/test-lib-summary.R
@@ -0,0 +1,17 @@
+test_that("lib_summary returns expected results", {
+  result <- lib_summary()
+
+  expect_s3_class(result, "data.frame")
+  expect_equal(ncol(result), 2)
+  expect_gte(nrow(result), 1)
+  expect_equal(names(result), c("Library", "n_packages"))
+  expect_type(result$n_packages, "integer")
+  expect_type(result$Library, "character")
+})
+
+#testing a side effect
+test_that("lib_summary that it fails appropriately", {
+  expect_error(lib_summary("hello"), "unused argument") #we expect the error to return that statement
+  #expect_error(lib_summary("hello"), "i am a stupid error") #this is what the error would return, which does not fit my test, which will fail upon testing
+})
+
