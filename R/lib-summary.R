642f904a1712eb8fac69d90d8557fa56c6fb35d1
AlexTheb <theb8@hotmail.com>
2024-11-05 13:28:59 -0500

    add fs dependency, add sizes parameter

--- R/lib-summary.R
+++ R/lib-summary.R
@@ -1,15 +1,32 @@
 #' provide number of r packages by lirbary
 #'
+#' @param sizes should sizes of libraries be calculated, default is false
+#'
 #' @return a data frame of R
 #' @export
 #'
 #'
 #' @examples
 #' lib_summary()
-lib_summary <- function() {
+lib_summary <- function(sizes= FALSE) {
+  if (!is.logical(sizes)){
+    stop("sizes must be logical (TRUE or FALSE)")
+  }
+
   pkgs <- utils::installed.packages()
   pkg_tbl <- table(pkgs[, "LibPath"])
   pkg_df <- as.data.frame(pkg_tbl, stringsAsFactors = FALSE)
   names(pkg_df) <- c("Library", "n_packages")
+  pkg_df
+
+  if (isTRUE(sizes)){
+    pkg_df$lib_size <- vapply(
+      pkg_df$Library,
+      function(x){
+        sum(fs::file_size(fs::dir_ls(x, recurse = T)))
+      },
+      FUN.VALUE=numeric(1)
+    )
+  }
   pkg_df
 }
--- tests/testthat/test-lib-summary.R
+++ tests/testthat/test-lib-summary.R
@@ -1,4 +1,4 @@
-test_that("lib_summary returns expected results", {
+test_that("lib_summary returns expected results, with default", {
   result <- lib_summary()
 
   expect_s3_class(result, "data.frame")
@@ -9,9 +9,22 @@ test_that("lib_summary returns expected results", {
   expect_type(result$Library, "character")
 })
 
+test_that("sizes work", {
+  result <- lib_summary(sizes=T)
+
+  expect_s3_class(result, "data.frame")
+  expect_equal(ncol(result), 3)
+  expect_gte(nrow(result), 1)
+  expect_equal(names(result), c("Library", "n_packages", "lib_size"))
+  expect_type(result$n_packages, "integer")
+  expect_type(result$Library, "character")
+  expect_type(result$lib_size, "double")
+})
+
 #testing a side effect
 test_that("lib_summary that it fails appropriately", {
-  expect_error(lib_summary("hello"), "unused argument") #we expect the error to return that statement
+  #expect_error(lib_summary("hello"), "unused argument") #we expect the error to return that statement
   #expect_error(lib_summary("hello"), "i am a stupid error") #this is what the error would return, which does not fit my test, which will fail upon testing
+  expect_error(lib_summary("hello"), "must be logical")
 })
 
